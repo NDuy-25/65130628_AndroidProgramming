a498b6aad50f0e75472cb1fcbdf8c043329d5775
NDuy-25 <duy540236@gmail.com>
2026-03-02 21:07:27 +0700

    Create Task.java

---
+++ DemoAndroid Studio/app/src/main/java/com/duy/mobileapp/Task.java
@@ -0,0 +1,11 @@
+package com.duy.mobileapp;
+public class Task {
+    public String id;
+    public String text;
+    public String time;
+    public boolean done; // trạng thái hoàn thành
+
+    public Task() {}
+}
+
+
