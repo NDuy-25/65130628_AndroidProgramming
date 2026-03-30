a9299cbd7371308b4f96607ef69238b2791c5ba9
NDuy-25 <duy540236@gmail.com>
2026-03-30 21:05:31 +0700

    Thử Nghiệm làm web caffe online

---
+++ BaiTapCongDiem/DemoWebCoffee/app/src/main/java/com/duy/a12coffee/MainActivity.java
@@ -0,0 +1,31 @@
+package com.duy.a12coffee;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        // Mặc định hiển thị HomeFragment khi mới mở app
+        if (savedInstanceState == null) {
+            replaceFragment(new HomeFragment());
+        }
+    }
+
+    public void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.content_frame, fragment);
+        fragmentTransaction.commit();
+    }
+}
